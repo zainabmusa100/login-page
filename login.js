24407a58a03a968c4a1fa6ef261dd24ed572bcff
zainabmusa100 <113892803+zainabmusa100@users.noreply.github.com>
2022-10-04 15:44:17 +0100

    add login page

---
+++ login.js
@@ -0,0 +1,21 @@
+
+	function validateform(){
+		var email = document.getElementById("form");
+		var password = document.getElementById("formb");
+
+		if (email.value !== "zainab") {
+			alert("email is incorrect");
+			return false;
+			
+		} else if (password.value !=="musa") {
+			alert("password is incorrect");
+			return false;
+		} else{
+			true;
+		}
+		} 
+
+		
+			 
+			
+	
